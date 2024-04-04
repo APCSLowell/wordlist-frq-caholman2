0d745f587aa480163ff7900d95196f0ece6528e5
caholman2 <142835012+caholman2@users.noreply.github.com>
2024-04-04 11:41:04 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -10,13 +10,22 @@ public class WordList
 
   public int numWordsOfLength(int len)
   {
-    /* to be implemented in part (a) */
-    
+    int result = 0;
+    for(int i=0; i<myList.size(); i++){
+      if(myList.get(i).length() == len){
+        result++;
+      }
+    }
+    return result;
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
-
+    for(int i=0; i<myList.size(); i++){
+      if(myList.get(i).length() == len){
+        myList.remove(i);
+        i--;
+      }
+    }
   }
 }
